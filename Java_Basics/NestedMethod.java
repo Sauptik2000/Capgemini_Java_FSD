cb3ecb8d5759427c87999d293df14d228bafd2be
Sauptik2000 <101315127+Sauptik2000@users.noreply.github.com>
2022-03-10 12:37:06 +0530

    Add files via upload

---
+++ Java_Basics/NestedMethod.java
@@ -0,0 +1,23 @@
+package com.cg;
+
+public class NestedMethod {
+	static int x = 1;
+	private int y = 3;
+	public void method1(int x)
+	{
+		NestedMethod t = new NestedMethod();
+		this.x = 2;
+		y = 4;
+
+		System.out.println("Test.x: " + NestedMethod.x);
+		System.out.println("t.x: " + t.x);
+		System.out.println("t.y: " + t.y);
+		System.out.println("y: " + y);
+	}
+
+	public static void main(String args[])
+	{
+		NestedMethod t = new NestedMethod();
+		t.method1(5);
+	}
+}
